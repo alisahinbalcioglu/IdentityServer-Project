0cf6149555e5e090f986829e6f288b9467f1fade
Ali şahin BALCIOĞLU <balcioglualisahin@gmail.com>
2022-05-30 19:57:53 +0300

    Proje dosyası ekle.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Akilliphone.IdentityServer.Dtos;
+using Akilliphone.IdentityServer.Models;
+using Akilliphone.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akilliphone.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUpDto.UserName,
+                Email = signUpDto.Email,
+                City = signUpDto.City,
+            };
+
+            var result = await _userManager.CreateAsync(user,signUpDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(ResponseDto<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+
+            return NoContent();
+        }
+    }
+}
